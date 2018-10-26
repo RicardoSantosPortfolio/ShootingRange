26434c339917c9e92a58828b7585a5b302d3d870
Ricardo Santos <ricardo.c.r.santos@gmail.com>
2018-10-26 13:54:03 -0300

    COMMIT 26.10.18 - Versão desktop finalizada

---
+++ Source/ShootingRange/Public/CToCSV.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CToCSV.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGRANGE_API UCToCSV : public UObject
+{
+	GENERATED_BODY()
+	
+	// Funcão para inicializar o arquivo CSV
+	//Função para escrever os dados no arquivo CSV; vem da blueprint uma string com todos os dados a serem escritos
+	
+	
+};
